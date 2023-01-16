c08a682b972557d1289e8e06ca85e13f2a514198
oussama-latrache <oussamatazilatrache@gmail.com>
2023-01-16 13:38:42 +0000

    uploading new notion website templates updates

---
+++ index.js
@@ -0,0 +1,166 @@
+let lftPoin = document.querySelector('.menu')
+let nav = document.querySelector('nav')
+let close = document.querySelector('.close')
+
+lftPoin.onclick = function (){
+    nav.classList.add("open")
+}
+close.onclick = function (){
+    nav.classList.remove("open")
+}
+document.onkeyup = function (e) {
+    if (e.Key === "Escape"){
+        nav.classList.remove("open")
+    }
+}
+let up = document.querySelector('.topbtn')
+window.onscroll = function () {
+    if(window.scrollY >= 700){
+      up.style.cssText = 'display:block;'
+    }else{
+      up.style.cssText = 'display:none;'
+    }
+  }
+  
+  
+  up.onclick = function (){
+    window.scrollTo({
+      top:0,
+      left:0,
+      behavior:'smooth'
+    })
+  }
+
+//   cursor
+
+var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + 'px';
+  cursorinner.style.top = y + 'px';
+});
+
+document.addEventListener('mousedown', function(){
+  cursor.classList.add('click');
+  cursorinner.classList.add('cursorinnerhover')
+});
+
+document.addEventListener('mouseup', function(){
+  cursor.classList.remove('click')
+  cursorinner.classList.remove('cursorinnerhover')
+});
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline()
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+  const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "black",
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+
+
+// template switcher
+let switcherLis = document.querySelectorAll(".swithcer li")
+let imgs = document.querySelectorAll('.template .card')
+let box = document.querySelectorAll('.template .contain .card')
+
+switcherLis.forEach((li) => {
+    li.addEventListener('click',removeActive)
+    li.addEventListener('click',manageImgs)
+})
+// remove active class from all lis and add to curent 
+function removeActive() {
+    switcherLis.forEach((li) => {
+        li.classList.remove("active")
+        this.classList.add('active')
+    })
+}
+// manage images
+function manageImgs(){
+    imgs.forEach((el)=>{
+        el.style.display = 'none'
+    })
+    document.querySelectorAll(this.dataset.filteer).forEach((el)=>{
+        el.style.display = 'block'
+    })
+}
